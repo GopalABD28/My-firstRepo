9a1d4a3a75260c50d7dd673bc27921114e6fa30a
Krishna <Krishna322@gmail.com>
2026-09-08 13:03:38 +0000

    Hello world code added

---
+++ app.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        // This prints the text inside the quotes to the screen
+        System.out.println("Hello, World!");
+    }
+}
+
